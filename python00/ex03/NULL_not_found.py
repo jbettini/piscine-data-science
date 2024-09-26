3af9a9c59d2410d0735176be59e3e04f9bdeba34
jbettini <ashhxtm@gmail.com>
2024-09-26 17:36:46 -0400

    ex03 done

---
+++ python00/ex03/NULL_not_found.py
@@ -0,0 +1,19 @@
+def is_nan(x):
+    return x != x
+
+def NULL_not_found(object: any) -> int:
+    match object:
+        case None:
+            print(f"Nothing: {object} {type(object)}")
+        case 0:
+            print(f"Zero: {object} {type(object)}")
+        case "":
+            print(f"Empty: {object} {type(object)}")
+        case False:
+            print(f"Fake: {object} {type(object)}")
+        case float() if is_nan(object):
+            print(f"Cheese: {object} {type(object)}")
+        case _:
+            print(f"Type not found")
+            return 1
+    return 0
