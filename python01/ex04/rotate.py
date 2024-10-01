d7096ac7c5a93706cbf0f9832108210e96bfaa92
jbettini <ashhxtm@gmail.com>
2024-10-01 09:13:40 +0200

    day01/ex04 done

--- python01/ex04/rotate.py
+++ python01/ex04/rotate.py
@@ -2,6 +2,7 @@ import array
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from load_image import ft_load
+import numpy as np
 
 mpl.rcParams['toolbar'] = 'None'
 
@@ -18,19 +19,35 @@ Used to quickly visualize part of the image array content.
         print(f"[{row[0]}]")
 
 
-def zoom(path: str):
+def ft_transpose(img: array):
+    '''
+Rotates the image by 90 degrees clockwise.
+'''
+    rows = len(img)
+    cols = len(img)
+    new = np.zeros((rows, cols))
+    for idx, row in enumerate(img):
+        for idx2, pix in enumerate(row):
+            new[idx2][idx] = pix
+    return new
+
+
+def rotate(path: str):
     '''
 Loads an image, slices a region, and displays,
-a zoom on the first channel in grayscale.
+on the first channel in grayscale with 90 degrees clockwise.
 '''
     img = ft_load(path)
-    print(f"The shape of image is: {img.shape}")
-    print_channel(img)
     sliced = img[100:500, 450:850]
     new = sliced[:, :, 0]
-    print(f"The shape after slicing : \
+    print(f"The shape of image is: \
 ({len(new)}, {len(new[0])}, 1) or {new.shape}")
     print_channel(new)
+    new = ft_transpose(new)
+    print(f"New shape after Transpose: {new.shape}")
+    print(f"[{new[0][:3].astype(int)} ... \
+{new[0][-3:].astype(int)}\n...\n{new[-1][:3].astype(int)} \
+... {new[-1][-3:].astype(int)}]")
     plt.imshow(new, cmap=plt.cm.gray)
     plt.show()
 
@@ -40,7 +57,7 @@ def main():
 Example usage of zoom function
 '''
     try:
-        zoom("../Ressources/animal.jpeg")
+        rotate("../Ressources/animal.jpeg")
     except AssertionError as e:
         print(f"{e}")
     except BaseException as e:
