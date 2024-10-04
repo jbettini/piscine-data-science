c73d22c35aa51e3a6c1ec7184611d4f89acc9c09
jbettini <ashhxtm@gmail.com>
2024-10-04 06:55:55 +0200

    day02/ex01 done

---
+++ python02/ex01/aff_life.py
@@ -0,0 +1,31 @@
+from pandas import DataFrame
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def display_country(country: str, df: DataFrame):
+    data = df[df['country'] == 'France']
+    if data.empty:
+        print(f"No data found for {country}")
+        return
+    years = list(data.columns[1:])
+    values = data.iloc[0, 1:].values
+    plt.plot(years, values, color='g')
+    plt.xlabel('Years')
+    plt.ylabel('Life Expectancy')
+    plt.title(f"{country} Life Expectancy Projections")
+    plt.xticks(years[::40])
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    try:
+        df = load("../Ressources/life_expectancy_years.csv")
+        display_country("France", df)
+    except BaseException as e:
+        print(f"An exception has been catch: {type(e).__name__}.")
+
+
+if __name__ == "__main__":
+    main()
